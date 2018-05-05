cecf1725021e304283c48f0c675058d754841c02
Adrienne Schueller <atschueller@gmail.com>
2018-05-05 17:49:17 -0400

    more js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,44 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var inquirer = require('inquirer');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'Atschueller',
-  password : '',
-  database : 'Bamazon'
+  host: 'localhost',
+  user: 'Atschueller',
+  password: '',
+  database: 'Bamazon'
 });
- 
+
 connection.connect();
- 
+
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
- 
-connection.end();
 
-inquirer.prompt([/* Pass your questions in here */]).then(answers => {
-    // Use user feedback for... whatever!!
+connection.end();
+//create a function to display products when loaded//
+function createProduct() {
+
+};
+
+inquirer.prompt([{
+  type: "input",
+  message: "What is the ID of the product you would like to purchase?",
+  id: "id",
+},
+{
+  type: "input",
+  message: "How many would you like to buy?",
+  quantity: "quantity",
+},
+]).then(answers => {
+  console.log(id);
+  console.log(quantity);
+  if (quantity >= stock_quantity) {
+    console.log('Your purchase of' + product_name + 'is complete!')
+
+  } else {
+    console.log('Insufficient Quantity!');
+  };
+  
+  
 });
