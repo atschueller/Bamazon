d1c47ce379f0aab42b5b853f565d88fbae630c26
Adrienne Schueller <atschueller@gmail.com>
2018-05-05 16:52:10 -0400

    msql and js starting code

---
+++ bamazonCustomer.js
@@ -0,0 +1,21 @@
+var mysql      = require('mysql');
+var inquirer = require('inquirer');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'Atschueller',
+  password : 'Chilipepper1!',
+  database : 'Bamazon'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
+
+inquirer.prompt([/* Pass your questions in here */]).then(answers => {
+    // Use user feedback for... whatever!!
+});
